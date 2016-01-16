9a01b76ad5d02a75d6540d74ec49e3207b36f6f1
yhkwok <kwo13001@byui.edu>
2016-01-16 13:18:26 -0700

    HardCodeClassifier 2.0 w/ Jacob

--- hardCodeClassifier2.py
+++ hardCodeClassifier2.py
@@ -5,28 +5,21 @@ iris = datasets.load_iris()
 
 #create the array to work with
 theArray = []
-for i, data in enumerate(iris.data):
-    targets = i/50
-    tempArray = [data, targets]
+for i, iris.data in enumerate(iris.data):
+    if i <= 49:
+        targets = 0
+    elif i <= 99 and i > 49:
+        targets = 1
+    else:
+        targets = 2
+    tempArray = [iris.data, targets]
     theArray.append(tempArray)
 #shuffle it
 random.shuffle(theArray)
 #split it
+print(theArray)
 trainset = theArray[:105] # 70%
 testset = theArray[105:] # 30%
-
-# Show the data (the attributes of each instance)
-print ("Data: ")
-print(iris.data)
-# Show the target values (in numeric format) of each instance
-print ("Target value: ")
-print(iris.target)
-# Show the actual target names that correspond to each number
-print ("Actual Target Names: ")
-print(iris.target_names)
-#whole iris
-print ("Whole iris: ")
-print(iris)
 
 class HardCoded(object):
     def __init__(self):
@@ -36,7 +29,7 @@ class HardCoded(object):
 
     def predict(self, testset):
         predictions = []
-        for i, data in enumerate(testset)
+        for i, data in enumerate(testset):
             predictions.append(0)
         return predictions
     def evaluate(self, predictions, testset):
@@ -55,7 +48,10 @@ classifier = HardCoded()
 classifier.train(trainset)
 #make prediction
 predictions = classifier.predict(testset)
+#print(predictions)
 #eva
 percentage = classifier.evaluate(predictions, testset)
-print("%.2f" % (percentage * 100)) + "%"
+percentage *= 100
+print(percentage)
+#print("%.2f" % percentage) + "%"
 
