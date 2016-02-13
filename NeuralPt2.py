3892b7cedba5785624c48eb25f2bda1e98331491
yhkwok <kwo13001@byui.edu>
2016-02-13 14:31:51 -0700

    Neural Part 2

--- NeuralPt2.py
+++ NeuralPt2.py
@@ -1,20 +1,26 @@
-from sklearn import datasets
-from sklearn.cross_validation import train_test_split
-from sklearn.metrics import accuracy_score
-from pandas import DataFrame, read_csv
-import pandas as pd
-from collections import Counter
-from sklearn import preprocessing
-import numpy as np
 import random
-
-##Collaborated with Jared and Robbie
+import csv
+import sys
+import numpy as np
+from scipy.stats import mode
+from sklearn.cross_validation import train_test_split
+from scipy.stats import entropy as entropy
+from sklearn.metrics import accuracy_score
+from sklearn import preprocessing
+import pandas as pd
+from collections import defaultdict
+import sys
+import scipy.signal as sig
+import itertools
 
 
 class Dataset:
     def __init__(self, csv, test_size, random_state, convert_nominal=False):
+
+        #Read in the csv
         self.ds = pd.read_csv(csv, header=None)
-        ds_column_len = len(self.ds.columns)
+        #get the  column length
+        ds_num_col = len(self.ds.columns)
 
         self.data_train = []
         self.data_test = []
@@ -23,30 +29,26 @@ class Dataset:
         self.std_train = []
         self.std_test = []
 
-        self.data = self.ds.loc[:, : ds_column_len - 2]
-        self.targets = self.ds[ds_column_len - 1]
+        #print("The dataframe before: \n", self.ds)
+        #if the user wants nominal data, divide any columns without strings to 4 buckets and assign them nominal values (0, 1, 2, 3)
+        if convert_nominal is True:
+            self.convert_numerical_data(self.ds)
+        else: #otherwise convert any string data to numbers
+            self.convert_nominal_data(self.ds)
+       # print("The dataframe after: \n", self.ds)
 
-
+        self.data = self.ds.loc[:, : ds_num_col - 2]
+        self.targets = self.ds[ds_num_col - 1]
 
         self.data_train, self.data_test, self.target_train, self.target_test = train_test_split(
                 self.data, self.targets, test_size=test_size, random_state=random_state)
+        # standardize the data if we are using numerical data
+        if not convert_nominal:
+            self.standardize_data()
         self.std_train = [0] * int(len(self.data_train))
         self.std_test = [0] * int(len(self.data_test))
 
-        print("The dataframe before: \n", self.ds)
-        if convert_nominal is True:
-            self.convert_numerical_data(self.ds)
-        else:
-            self.convert_nominal_data(self.ds)
-            self.standardize_data()
-        print("The dataframe after: \n", self.ds)
-        # This function uses a zscore to standardize the data
-
-        if not convert_nominal:
-            self.standardize_data()
-
-
-        print("train data and targets", self.data_train, self.target_train)
+       #print("train data and targets", self.data_train, self.target_train)
 
     # This function turns nominal data into number values that are not scaled
     def convert_nominal_data(self, dataset):
@@ -81,6 +83,9 @@ class Dataset:
             else:
                dataset[i] = pd.qcut(dataset[i], 4, labels=False)
 
+
+
+    # This function uses a zscore to standardize the data
     def standardize_data(self):
         # fill std_train with standardized values
         self.std_train = (self.data_train - self.data_train.mean()) / self.data_train.std()
@@ -88,65 +93,146 @@ class Dataset:
         self.std_test = (self.data_test - self.data_train.mean()) / self.data_train.std()
 
 class NeuralNetwork:
-    def __init__(self,learning_rate, data, targets, inputs, num_layers=3):
+    def __init__(self, learning_rate, data, targets, inputs, num_layers=3):
         self.learning_rate = learning_rate
         self.data = data
+        self.bias = -1
         self.targets = targets
-        self.inputs = inputs
         self.labels = np.unique(targets)
-        num_inputs = len(self.inputs.columns)
-        self.input_list = []
+        self.inputs = inputs
         self.neurons = []
+        self.num_inputs = len(self.data.columns)
+        self.input_list = []
+        self.layers = []
+        #self.label_dict = defaultdict(list)
+        print("data_inputs: \n", self.data)
         self.build_network(num_layers)
-        # for row in self.inputs.iterrows:
 
-    # def calc_random_wieght(self, num_weights):
-    #     print("printing random numbers")
-    #     return random.uniform(-1.0, 1.0)
+    # build_network will build the network, the network will be stored in a multidimentional array
+    # A network with 3 input neurons, 1 layer of 2 hidden neurons and 4 output neurons would be stored like this:
+    # [[Neuron, Neuron, Neuron], [Neuron, Neuron], [Neuron, Neuron, Neuron, Neuron]]
+    # The initial layer will be built with the number of inputs of the dataset and the rest will be built with the
+    # number of neurons from the previous layer
+    def build_network(self, num_layers):
+        #The initial inputs for layer 1 will be the number of inputs of the data
+        layer_inputs = self.num_inputs
+        # if the user has specified more than 1 layer
+        if num_layers > 1:
+            #loop through the layers except the last layer which we will handle separately
+            for i in range(num_layers - 1):
+                this_layer = []
+                #prompt user for num neurons in this layer
 
-    def build_nework(self, num_layers):
+                #num_neurons = int(input('Enter number of neurons: '))
+                num_neurons = 2
+                for j in range(num_neurons):
+                    #make that many neurons in the layer
+                    new_neuron = self.Neuron(layer_inputs)
+                    this_layer.append(new_neuron)
+                #grab the number of neurons to use as layer inputs for our next layer
+                layer_inputs = num_neurons
+                #append that layer to the NN layers array
+                self.layers.append(this_layer)
+        # this will either be the last layer of our MLP or a single layer
+        last_layer = []
+        for label in self.labels:
+            new_neuron = self.Neuron(layer_inputs)
+            last_layer.append(new_neuron)
+        self.layers.append(last_layer)
 
-    def train_netwrok(self):
-        print("random wieghts")
-        for row in self.inputs.iterrows():
-            print("row")
-            for nueron in self.neurons:
-                activation = nueron.calculate_activation(row)
-                print(activation)
+    def train_network(self):
+        network_output = []
+        counter = 0
+        #loop through each row
+        for data_inputs in self.data.iterrows():
+            outputs = []
+            outputs2 = []
+            iteration_inputs = data_inputs
 
+            for layer in (self.layers):
+                outputs = []
 
+                for neuron in layer:
+                    counter += 1
+                    outputs.append(neuron.compute_activation(iteration_inputs))
+                iteration_inputs = (1, outputs)
+
+            network_output.append(outputs)
+
+            network_max_outputs = []
+            for item in itertools.chain(network_output):
+                network_max_outputs.append(np.argmax(item))
+
+        print(network_max_outputs)
+
+        print(network_output, "COUNTER: ", counter)
+        percentage = []
+        values = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2]
+        value1 = values.count(0)
+        value2 = values.count(1)
+        value3 = values.count(2)
+
+        part2 = network_max_outputs.count(1)
+        part3 = network_max_outputs.count(2)
+        part1 = network_max_outputs.count(0)
+
+        part = part1 + part2 +part3
+        print("counted numebr", part)
+        temp = set(values)
+        # counter2 = 0
+        # for item in network_max_outputs:
+        #     final = []
+        #     if item in temp:
+        #         counter2 = counter2 + 1
+        #         print(counter2)
+        #         #print("{0} is in values".format(item))
+        #
+        # print(values)
+        # print(network_max_outputs)
+        # print(counter2)
+
+        return network_max_outputs
 
     class Neuron:
-        def __init__(self, num_inputs, label):
-            self.threshold = 0
-            self.label = label
+        def __init__(self, num_inputs):
+            self.rangemax = 1.0
+            self.rangemin = -1.0
             self.inputs = []
-            self.range_max = 1.0
-            self.range_min = -1.0
-            self.bias = -1
+
             for i in range(num_inputs):
-                self.inputs.append(self.Input(i, random.uniform(self.range_min, self.range_max)))
+                print("appending input number: ", i)
+                self.inputs.append(self.Input(random.uniform(self.rangemin, self.rangemax)))
+            #make bias input the last input
+            self.inputs.append(self.Input(random.uniform(self.rangemin, self.rangemax)))
+            print("making a neuron with ", num_inputs, " inputs\n")
 
-        def calculate_activation(self, input):
-            sum = 0
-            for i in range(len(self.inputs)):
-                sum += self.inputs[i].weight * input[1][i]
-                sum *= self.bias * self.inputs[i].weight
+        def compute_activation(self, input):
+            #assert input is len(self.inputs)
+            _sum = 0
+            #loop through all inputs
+            #print("for compute we are looping through range: ", range(len(self.inputs) - 1))
+            for i in range(len(self.inputs) -1):
+                _sum += self.inputs[i].weight * input[1][i]
+            #calculate for bias input -1 gets last element in an array
+            _sum += self.inputs[-1].weight * 1
+            score = self.sigmoid(_sum)
 
-            if sum > self.threshold:
-                return 1
-            else:
-                return 0
+            return score
+
+        def sigmoid(self, x):
+            return 1 / (1 + np.exp(x))
+
+        def dsigmoid(self, y):
+            return y(1.0 - y)
 
         class Input:
-            def __init__(self, attribute, weight):
-                self.attribute = attribute
+            def __init__(self, weight):
                 self.weight = weight
 
+print("**********************CSV**************************")
 
-the_data_set = Dataset('pima-indians-diabetes.csv', .3, 42, False)
+my_dataset = Dataset('iris2', .3, 3333, False)
 
-neural = NeuralNetwork(3, the_data_set.data_train, the_data_set.target_train, the_data_set.data_test)
+nn = NeuralNetwork(.3, my_dataset.data_train, my_dataset.target_train, my_dataset.data_test)
 
-neural.train_netwrok()
-#output = decisionTree.get_highest_entropy(the_data_set.data)
+nn.train_network()
